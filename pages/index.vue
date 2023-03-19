da192fe83079057fb1b4d477de58067224b75525
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-03-20 03:02:56 +0900

    レイアウト修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
-<template>
-  <p>hogehoge</p>
-</template>
-
 <script setup>
+// const topics = ref(['photograph', 'engineering'])
 </script>
 
-<style scoped>
+<template>
+  <div class="works">
+    <nuxt-link to="/photograph" class="photograph">
+      <h2>PHOTOGRAPH</h2>
+    </nuxt-link>
+    <nuxt-link to="/engineering" class="engineering">
+      <h2>ENGEERING</h2>
+    </nuxt-link>
+  </div>
 
+</template>
+
+<style scoped>
+.works {display: flex;justify-content: space-evenly;margin-top: 6rem;}
+:where(.photograph, .engineering) {width: 45%;height: 30rem;display: flex;}
+.engineering {background-color: blue;display: grid;place-items: center;color:aliceblue;}
+.photograph {background-color: deeppink;display: grid;place-items: center;color:aliceblue;}
 </style>
