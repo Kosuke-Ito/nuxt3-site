2a8fe2883a7c9c2c9db0e9af44db40e771822632
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-03-21 20:21:39 +0900

    デザイン調整

--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,34 @@
 </template>
 
 <style scoped>
-.works {display: flex;justify-content: space-evenly;margin-top: 6rem;}
-:where(.photograph, .engineering) {width: 45%;height: 30rem;display: flex;}
-.engineering {background-color: blue;display: grid;place-items: center;color:aliceblue;}
-.photograph {background-color: deeppink;display: grid;place-items: center;color:aliceblue;}
+.works {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 6rem;
+}
+:where(.photograph, .engineering) {
+  width: 45%;
+  height: 30rem;
+  display: flex;
+  text-decoration: none;
+  color: aliceblue;
+  background-size: cover;
+  background-position: center;
+}
+.engineering {
+  background-color: blue;
+  display: grid;
+  place-items: center;
+  background-image: url("~/assets/engineering.jpg");
+}
+.photograph {
+  background-color: deeppink;
+  display: grid;
+  place-items: center;
+  background-image: url("~/assets/photograph.jpg");
+}
+.works h2 {
+  background-color: rgba(1, 1, 1, 0.5);
+  padding: .75rem 1.5rem;
+}
 </style>
